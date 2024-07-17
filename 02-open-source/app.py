89bcb541f9975dd84118f3185d69148a3aeb6517
Axel Ef <olek4don@gmail.com>
2024-07-18 00:39:24 +0200

    create Streamlit app for our RAG system

---
+++ 02-open-source/app.py
@@ -0,0 +1,137 @@
+import time
+
+import streamlit as st
+from elasticsearch import Elasticsearch
+from openai import OpenAI
+
+client = OpenAI(
+    base_url="http://localhost:11434/v1/",
+    api_key="ollama",
+)
+es_client = Elasticsearch("http://localhost:9200")
+
+index_settings = {
+    "settings": {"number_of_shards": 1, "number_of_replicas": 0},
+    "mappings": {
+        "properties": {
+            "text": {"type": "text"},
+            "section": {"type": "text"},
+            "question": {"type": "text"},
+            "course": {"type": "keyword"},
+        }
+    },
+}
+
+index_name = "course-questions"
+
+
+def elastic_search(query, index_name=index_name):
+    search_query = {
+        "size": 5,
+        "query": {
+            "bool": {
+                "must": {
+                    "multi_match": {
+                        "query": query,
+                        "fields": ["question^3", "text", "section"],
+                        "type": "best_fields",
+                    }
+                },
+                "filter": {"term": {"course": "data-engineering-zoomcamp"}},
+            }
+        },
+    }
+
+    response = es_client.search(index=index_name, body=search_query)
+
+    result_docs = []
+
+    for hit in response["hits"]["hits"]:
+        result_docs.append(hit["_source"])
+
+    return result_docs
+
+
+def build_prompt(query, search_results):
+    prompt_template = """
+You're a course teaching assistant. Answer the QUESTION based on the CONTEXT from the FAQ database.
+Use only the facts from the CONTEXT when answering the QUESTION.
+
+QUESTION: {question}
+
+CONTEXT: 
+{context}
+""".strip()
+
+    context = ""
+
+    for doc in search_results:
+        context = (
+            context
+            + f"section: {doc['section']}\nquestion: {doc['question']}\nanswer: {doc['text']}\n\n"
+        )
+
+    prompt = prompt_template.format(question=query, context=context).strip()
+    return prompt
+
+
+def llm(prompt):
+    response = client.chat.completions.create(
+        model="phi3", messages=[{"role": "user", "content": prompt}]
+    )
+
+    return response.choices[0].message.content
+
+
+def build_prompt(query, search_results):
+    prompt_template = """
+You're a course teaching assistant. Answer the QUESTION based on the CONTEXT from the FAQ database.
+Use only the facts from the CONTEXT when answering the QUESTION.
+
+QUESTION: {question}
+
+CONTEXT: 
+{context}
+""".strip()
+
+    context = ""
+
+    for doc in search_results:
+        context = (
+            context
+            + f"section: {doc['section']}\nquestion: {doc['question']}\nanswer: {doc['text']}\n\n"
+        )
+
+    prompt = prompt_template.format(question=query, context=context).strip()
+    return prompt
+
+
+def llm(prompt):
+    response = client.chat.completions.create(
+        model="phi3", messages=[{"role": "user", "content": prompt}]
+    )
+
+    return response.choices[0].message.content
+
+
+def rag(query):
+    search_results = elastic_search(query)
+    prompt = build_prompt(query, search_results)
+    answer = llm(prompt)
+    return answer
+
+
+def main():
+    st.title("Simple Streamlit App")
+
+    input_text = st.text_input("Enter your query here:")
+    ask_button = st.button("Ask")
+
+    if ask_button:
+        with st.spinner("Processing..."):
+            result = rag(input_text)
+            st.success(result)
+
+
+if __name__ == "__main__":
+    main()
